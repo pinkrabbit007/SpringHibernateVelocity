03f91ef901a3e209e24616bc62000caf9e0a4643
zhangjunqiao <350024806@qq.com>
2015-09-13 17:21:05 +0800

    课题组网页正式上线，http://10.13.54.141:8080/labweb/home.do即可访问。解决了网页编码中文乱码的问题，原因是html文件保存时应该保存为utf-8格式。我把张博html里面几个超链接比如members.html改成了members.do你不要去改回来还，只要改成.do格式的我都有用的。部署在q1机器上。Tomcat用的是7，java的jdk版本是1.8

--- src/main/java/shiyanlou/test/hibernate/controller/AcitivityController.java
+++ src/main/java/shiyanlou/test/hibernate/controller/AcitivityController.java
@@ -1,16 +1,14 @@
 package shiyanlou.test.hibernate.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("home.do")
-public class HomeController { 
+@RequestMapping("activities.do")
+public class AcitivityController {
 	@RequestMapping(method = { RequestMethod.POST, RequestMethod.GET })
 	public String login2() {
-		return "homepage";
+		return "activities";
 	}
 }
