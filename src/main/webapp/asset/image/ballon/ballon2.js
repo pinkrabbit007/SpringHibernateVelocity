3a75944ba6efe9b79d85264d561ceacdc11828f3
zhangjunqiao <350024806@qq.com>
2015-09-23 15:50:58 +0800

    添加气球效果by张博

---
+++ src/main/webapp/asset/image/ballon/ballon2.js
@@ -0,0 +1,47 @@
+﻿function moveElement(elementID,final_x,final_y,interval) {
+  if (!document.getElementById) return false;
+  if (!document.getElementById(elementID)) return false;
+  var elem2 = document.getElementById(elementID);
+  if (elem2.movement) {
+    clearTimeout(elem2.movement);
+  }
+  if (!elem2.style.left) {
+    elem2.style.left = "550px";
+  }
+  if (!elem2.style.top) {
+    elem2.style.top = "900px";
+  }
+  var xpos = parseInt(elem2.style.left);
+  var ypos = parseInt(elem2.style.top);
+  if (xpos == final_x && ypos == final_y) {
+    return true;
+  }
+  if (xpos < final_x) {
+    var dist = Math.ceil((final_x - xpos)/100);
+    xpos = xpos + dist;
+  }
+  if (xpos > final_x) {
+    var dist = Math.ceil((xpos - final_x)/100);
+    xpos = xpos - dist;
+  }
+  if (ypos < final_y) {
+    var dist = Math.ceil((final_y - ypos)/100);
+    ypos = ypos + dist;
+  }
+  if (ypos > final_y) {
+    var dist = Math.ceil((ypos - final_y)/100);
+    ypos = ypos - dist;
+  }
+  elem2.style.left = xpos + "px";
+  elem2.style.top = ypos + "px";
+  var repeat = "moveElement('"+elementID+"',"+final_x+","+final_y+","+interval+")";
+  elem2.movement = setTimeout(repeat,interval);
+}
+function ballon2(){
+	document.getElementById("img2").style.visibility="visible";
+  var elem2 = document.getElementById("img2");
+  elem2.style.position = "fixed";
+  elem2.style.left = "550px";
+  elem2.style.top = "900px";
+  moveElement("img2",550,-250,20);
+}
---
+++ src/main/webapp/asset/image/ballon/ballon3.js
@@ -0,0 +1,47 @@
+﻿function moveElement(elementID,final_x,final_y,interval) {
+  if (!document.getElementById) return false;
+  if (!document.getElementById(elementID)) return false;
+  var elem3 = document.getElementById(elementID);
+  if (elem3.movement) {
+    clearTimeout(elem3.movement);
+  }
+  if (!elem3.style.left) {
+    elem3.style.left = "100px";
+  }
+  if (!elem3.style.top) {
+    elem3.style.top = "900px";
+  }
+  var xpos = parseInt(elem3.style.left);
+  var ypos = parseInt(elem3.style.top);
+  if (xpos == final_x && ypos == final_y) {
+    return true;
+  }
+  if (xpos < final_x) {
+    var dist = Math.ceil((final_x - xpos)/100);
+    xpos = xpos + dist;
+  }
+  if (xpos > final_x) {
+    var dist = Math.ceil((xpos - final_x)/100);
+    xpos = xpos - dist;
+  }
+  if (ypos < final_y) {
+    var dist = Math.ceil((final_y - ypos)/100);
+    ypos = ypos + dist;
+  }
+  if (ypos > final_y) {
+    var dist = Math.ceil((ypos - final_y)/100);
+    ypos = ypos - dist;
+  }
+  elem3.style.left = xpos + "px";
+  elem3.style.top = ypos + "px";
+  var repeat = "moveElement('"+elementID+"',"+final_x+","+final_y+","+interval+")";
+  elem3.movement = setTimeout(repeat,interval);
+}
+function ballon3(){
+	document.getElementById("img3").style.visibility="visible";
+  var elem3 = document.getElementById("img3");
+  elem3.style.position = "fixed";
+  elem3.style.left = "100px";
+  elem3.style.top = "900px";
+  moveElement("img3",100,-250,20);
+}
