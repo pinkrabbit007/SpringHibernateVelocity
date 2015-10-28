c146b728b5522ee15545609aee7d08284abbe391
zhangjunqiao <350024806@qq.com>
2015-10-28 11:25:31 +0800

    增加机房历史和照片。改变温度传值string为float包装类。

--- src/main/java/shiyanlou/test/hibernate/controller/JfglController.java
+++ src/main/java/shiyanlou/test/hibernate/controller/JfglController.java
@@ -39,37 +39,54 @@ public class JfglController {
 			temperature = "Cannot connect to email-system " + e.toString();
 			e.printStackTrace();
 		}
-		mav.addObject("lab429temperature", temperature);
+		mav.addObject("lab427temperature", temperature);
 
 		/**
 		 * 获取过去24小时的温度
 		 */
 
-		Vector<String> result = null;
+		Vector<String> primaryresult = null;
+		Vector<Float> result = new Vector<Float>();
 		try {
-			result = ReciveOneMail.find24hoursTemper();
-			for (int i = 0; i < result.size(); i++) {
-				System.out.print(" " + result.get(i));
+			primaryresult = ReciveOneMail.find24hoursTemper();
+			for (int i = 0; i < primaryresult.size(); i++) {
+				System.out.print(" " + primaryresult.get(i));
+				Float num = new Float(primaryresult.get(i));
+				// Float num = Float.valueOf(primaryresult.get(i));
+				// //好像这句也是对的，那么new一个包装类和这个有啥区别呢？
+				result.add(num);
 			}
 
 		} catch (MessagingException e) {
 			System.out.println("We do not have 144 emails !!");
 		}
 		mav.addObject("last24hoursT", result);
-		
-		mav.addObject("last24hoursT_0", result.get(0));mav.addObject("last24hoursT_1", result.get(1));
-		mav.addObject("last24hoursT_2", result.get(2));mav.addObject("last24hoursT_3", result.get(3));
-		mav.addObject("last24hoursT_4", result.get(4));mav.addObject("last24hoursT_5", result.get(5));
-		mav.addObject("last24hoursT_6", result.get(6));mav.addObject("last24hoursT_7", result.get(7));
-		mav.addObject("last24hoursT_8", result.get(8));mav.addObject("last24hoursT_9", result.get(9));
-		mav.addObject("last24hoursT_10", result.get(10));mav.addObject("last24hoursT_11", result.get(11));
-		mav.addObject("last24hoursT_12", result.get(12));mav.addObject("last24hoursT_13", result.get(13));
-		mav.addObject("last24hoursT_14", result.get(14));mav.addObject("last24hoursT_15", result.get(15));
-		mav.addObject("last24hoursT_16", result.get(16));mav.addObject("last24hoursT_17", result.get(17));
-		mav.addObject("last24hoursT_18", result.get(18));mav.addObject("last24hoursT_19", result.get(19));
-		mav.addObject("last24hoursT_20", result.get(20));mav.addObject("last24hoursT_21", result.get(21));
-		mav.addObject("last24hoursT_22", result.get(22));mav.addObject("last24hoursT_23", result.get(23)); 
-		
+
+		mav.addObject("last24hoursT_0", result.get(0));
+		mav.addObject("last24hoursT_1", result.get(1));
+		mav.addObject("last24hoursT_2", result.get(2));
+		mav.addObject("last24hoursT_3", result.get(3));
+		mav.addObject("last24hoursT_4", result.get(4));
+		mav.addObject("last24hoursT_5", result.get(5));
+		mav.addObject("last24hoursT_6", result.get(6));
+		mav.addObject("last24hoursT_7", result.get(7));
+		mav.addObject("last24hoursT_8", result.get(8));
+		mav.addObject("last24hoursT_9", result.get(9));
+		mav.addObject("last24hoursT_10", result.get(10));
+		mav.addObject("last24hoursT_11", result.get(11));
+		mav.addObject("last24hoursT_12", result.get(12));
+		mav.addObject("last24hoursT_13", result.get(13));
+		mav.addObject("last24hoursT_14", result.get(14));
+		mav.addObject("last24hoursT_15", result.get(15));
+		mav.addObject("last24hoursT_16", result.get(16));
+		mav.addObject("last24hoursT_17", result.get(17));
+		mav.addObject("last24hoursT_18", result.get(18));
+		mav.addObject("last24hoursT_19", result.get(19));
+		mav.addObject("last24hoursT_20", result.get(20));
+		mav.addObject("last24hoursT_21", result.get(21));
+		mav.addObject("last24hoursT_22", result.get(22));
+		mav.addObject("last24hoursT_23", result.get(23));
+
 		/**
 		 * 获取过去一周的温度
 		 */
@@ -84,13 +101,16 @@ public class JfglController {
 			System.out.println("We do not have 144 emails !!");
 		}
 		mav.addObject("lastWeekT", resultofweek);
-		
-		mav.addObject("lastWeekT_0", resultofweek.get(0));mav.addObject("lastWeekT_1", resultofweek.get(1));
-		mav.addObject("lastWeekT_2", resultofweek.get(2));mav.addObject("lastWeekT_3", resultofweek.get(3));
-		mav.addObject("lastWeekT_4", resultofweek.get(4));mav.addObject("lastWeekT_5", resultofweek.get(5));
-		mav.addObject("lastWeekT_6", resultofweek.get(6)); 
-		
+
+		mav.addObject("lastWeekT_0", resultofweek.get(0));
+		mav.addObject("lastWeekT_1", resultofweek.get(1));
+		mav.addObject("lastWeekT_2", resultofweek.get(2));
+		mav.addObject("lastWeekT_3", resultofweek.get(3));
+		mav.addObject("lastWeekT_4", resultofweek.get(4));
+		mav.addObject("lastWeekT_5", resultofweek.get(5));
+		mav.addObject("lastWeekT_6", resultofweek.get(6));
+
 		return mav;
 	}
-	 
+
 }
