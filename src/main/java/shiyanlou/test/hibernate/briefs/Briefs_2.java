a7599a42353519677a24ae6354b03bd788329f73
zhangjunqiao <350024806@qq.com>
2016-03-15 20:49:40 +0800

    增加李德兵郑静毕业新闻稿，更新李德兵郑静嵇陆怡介绍错误

---
+++ src/main/java/shiyanlou/test/hibernate/briefs/Briefs_2.java
@@ -0,0 +1,20 @@
+package shiyanlou.test.hibernate.briefs;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @date 2015-10-28
+ * @author zjq
+ * @content keith来访新闻报道
+ */
+
+@Controller
+@RequestMapping("briefs_2.do")
+public class Briefs_2 {
+	@RequestMapping(method = { RequestMethod.POST, RequestMethod.GET })
+	public String login2() {
+		return "briefs_2";
+	}
+}
