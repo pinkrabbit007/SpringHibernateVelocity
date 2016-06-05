269a4f82c7de9cf82d25420da9c6999eac8c816f
zhangjunqiao <350024806@qq.com>
2016-06-05 19:41:18 +0800

    张俊俏，康正中答辩

---
+++ src/main/java/shiyanlou/test/hibernate/briefs/Briefs_7.java
@@ -0,0 +1,20 @@
+package shiyanlou.test.hibernate.briefs;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @date 2016-4-12
+ * @author zjq
+ * @content zzs毕业答辩
+ */
+
+@Controller
+@RequestMapping("briefs_7.do")
+public class Briefs_7 {
+	@RequestMapping(method = { RequestMethod.POST, RequestMethod.GET })
+	public String login2() {
+		return "briefs_7";
+	}
+}
